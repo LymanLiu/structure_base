f9abca3e66323f119a7a576fd2af97b13a2c357b
LymanLiu <757325262@qq.com>
2017-01-29 09:17:12 +0800

    add manager

---
+++ src/multi/entries/manager/view.js
@@ -0,0 +1,32 @@
+import model from './model';
+import MultiView from '../../../core/MultiView';
+import Header from '../../logics/header/view.js';
+import Footer from '../../logics/footer/view.js';
+class View extends MultiView {
+    constructor() {
+        super();
+        this.comModel = model;
+        this.initModel();
+    }
+
+    initModel() {
+
+        // $('h1').click(this.sayTest);
+        
+        let header = new Header();
+        let footer = new Footer();
+
+
+        this.model = {...this.comModel,
+            ... {
+                title: '<$- HOME $>'
+            }
+        };
+
+        return this;
+    }
+
+
+}
+
+new View();
