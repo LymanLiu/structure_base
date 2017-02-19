1c1b196e8df607da7d877e2accf937176e773b2f
LymanLiu <757325262@qq.com>
2017-02-20 00:22:08 +0800

    temp

--- src/multi/logics/indexContainer/model.js
+++ src/multi/logics/indexContainer/model.js
@@ -29,27 +29,59 @@ module.exports = {
     				<li class="one">
                         <div class="img-box"><img src="/static/images/multi/entries/index/images/icon-ditu.png" alt=""></div>
 						<h4>更安全的运输路线</h4>
-						
+						<div class="isMask">
+                            <div class="mask-inner">
+                                <p>1 根据货物的数量和种类，为你提供最便捷的运输方式。</p>
+                                <p>2 空运，汽运，铁路，快递，海运，一站式服务。</p>
+                                <p>3 货物单号查询，让您随时随地掌握货物状态。</p>
+                            </div>
+                        </div>
 					</li>
 					<li class="two">
 						<div class="img-box"><img src="/static/images/multi/entries/index/images/icon-shixiao.png" alt=""></div>
                         <h4>更快的运输时效</h4>
-						
+						<div class="isMask">
+                            <div class="mask-inner">
+                                <p>1 空运 8-12天</p>
+                                <p>2 陆运 18-25天</p>
+                                <p>3 快递 72小时到莫斯科，门到门服务12-16天。</p>
+                            </div>
+                           
+                        </div>
 					</li>
 					<li class="three">
 						<div class="img-box"><img src="/static/images/multi/entries/index/images/icon-fuwu.png" alt=""></div>
                         <h4>更便捷的一站式服务</h4>
-						
+						<div class="isMask">
+                            <div class="mask-inner">
+                                <p>1 全程跟踪货物的状态</p>
+                                <p>2 快速的清关环节</p>
+                                <p>3 货物直达客户手中</p>
+                            </div>
+                           
+                        </div>
 					</li>
 					<li class="four">
 						<div class="img-box"><img src="/static/images/multi/entries/index/images/icon-kefu.png" alt=""></div>
                         <h4>更快的响应速度</h4>
-						
+						<div class="isMask">
+                            <div class="mask-inner">
+                               <p>1 全天侯24小时为你服务</p>
+                                <p>2  一个电话，轻松搞定发货中的烦恼</p>
+                            </div>
+                           
+                        </div>
 					</li>
 					<li class="five">
 						<div class="img-box"><img src="/static/images/multi/entries/index/images/icon-shouhou.png" alt=""></div>
                         <h4>更优质的售后服务</h4>
-						
+						<div class="isMask">
+                            <div class="mask-inner">
+                                <p>1 货物到达目的地，及时通知客户，提供仓储服务</p>
+                                <p>2 货物到达目的地，提供检查货物运输质量问题</p>
+                            </div>
+                           
+                        </div>
 					</li>
     			</ul>
     		`
